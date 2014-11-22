17908d75411b4a21aaaaaac2ddff0f6a82d5ab9a
AzureWolf <AzureWolf@JPro.home>
2014-11-22 08:11:45 -0500

    Complete steps 1-4 in project

---
+++ run_analysis.R
@@ -0,0 +1,41 @@
+## Grab raw data
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "wearabledata.zip", method = "curl");
+unzip("wearabledata.zip");
+file.remove("wearabledata.zip");
+
+### Grab universal variables
+experimentLabels <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE);
+columnLabels <- read.table("UCI HAR Dataset/features.txt", header = FALSE);
+
+### Function to get subjects, activity, and results together
+getCleanTable <- function(groupName) {
+	### Here is where step 2 is handled: Extracts only the measurements on the mean and standard deviation for each measurement.
+	filterResults <- function(results) {
+		wantedColumns <- grepl("-mean|-std", names(results));
+		results[,wantedColumns];
+	}
+
+	subjects <- read.table(paste("UCI HAR Dataset/", groupName, "/subject_", groupName, ".txt", sep = ""), header = FALSE, col.names = "Subject");
+	activity <- read.table(paste("UCI HAR Dataset/", groupName, "/y_", groupName, ".txt", sep = ""), header = FALSE, col.names = "Activity");
+	results <- read.table(paste("UCI HAR Dataset/", groupName, "/X_", groupName, ".txt", sep = ""), header = FALSE);
+
+	### Here is where step 4 is handled: Appropriately labels the data set with descriptive variable names.
+	### Step 2 is also called here
+	colnames(results) <- as.character(columnLabels[,2]);
+	results <- filterResults(results);
+
+	### Here is where step 3 is handled: Uses descriptive activity names to name the activities in the data set
+	activity <- factor(activity[,1]);
+	levels(activity) <- as.character(experimentLabels[,2]);
+
+	cbind(subjects,activity,results);
+}
+
+### Grab groups and merge them
+testGroup <- getCleanTable("test");
+trainGroup <- getCleanTable("train");
+
+## This is step 1 of assignment: Merges the training and the test sets to create one data set.
+combinedGroup <- rbind(testGroup,trainGroup)
+
