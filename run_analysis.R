645f594e0141171052e7c818e41702202aa5017c
AzureWolf <AzureWolf@JPro.home>
2014-11-23 01:59:17 -0500

    Code cleanup; save final tidydata to file

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-## Grab raw data
+## Grab raw data, remove zip
 
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "wearabledata.zip", method = "curl");
 unzip("wearabledata.zip");
@@ -21,7 +21,7 @@ getCleanTable <- function(groupName) {
 	results <- read.table(paste("UCI HAR Dataset/", groupName, "/X_", groupName, ".txt", sep = ""), header = FALSE);
 
 	### Here is where step 4 is handled: Appropriately labels the data set with descriptive variable names.
-	### Step 2 is also called here
+	### Step 2's function is also called here
 	colnames(results) <- as.character(columnLabels[,2]);
 	results <- filterResults(results);
 
@@ -36,27 +36,31 @@ getCleanTable <- function(groupName) {
 testGroup <- getCleanTable("test");
 trainGroup <- getCleanTable("train");
 
+### Remove data folder; no longer needed. For re-running purposes
+unlink("UCI HAR Dataset/", recursive = TRUE)
+
 ## This is step 1 of assignment: Merges the training and the test sets to create one data set.
 combinedGroup <- rbind(testGroup,trainGroup)
 
 ## This is step 5 of assignment: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 getMeany <- function(combinedGroup) {
 	sortedMeans <- data.frame();
-	rowNumbers <- ncol(combinedGroup);
+	colNumbers <- ncol(combinedGroup);
 	
 	for(subject in unique(combinedGroup$Subject)) {
 		values <- data.frame()
 		for(activity2 in unique(combinedGroup$activity)) {
-			values <- rbind(colMeans(subset(combinedGroup, Subject == subject & activity == activity2)[,3:rowNumbers]))
+			values <- rbind(colMeans(subset(combinedGroup, Subject == subject & activity == activity2)[,3:colNumbers]))
 			values <- cbind(activity2, values)
+			values <- cbind(subject, values);
+			sortedMeans <- rbind(sortedMeans, values);
 		}
-		values <- cbind(subject, values);
-		sortedMeans <- rbind(sortedMeans, values);
 	}
 
 	colnames(sortedMeans) <- names(combinedGroup);
-	final <- final[order(subject, activity)];
+	sortedMeans <- sortedMeans[with(sortedMeans, order(as.numeric(as.character(Subject)), activity)),];
 	sortedMeans;
 }
 
 tidySubjectActivity <- getMeany(combinedGroup);
+write.table(tidySubjectActivity, file = "tidySubjectActivity.txt", row.names = FALSE)
